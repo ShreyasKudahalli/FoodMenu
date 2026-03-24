bfd6890e7f9b3ea06306762b2f48e957491ba84d
ShreyasKudahalli <shreyaskudahalli1857@gmail.com>
2026-03-25 00:07:43 +0530

    food menu

--- RestaurantMenu/settings.py
+++ RestaurantMenu/settings.py
@@ -153,6 +153,10 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+# Login URL
+LOGIN_URL = 'login'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
