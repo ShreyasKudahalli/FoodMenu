4e3a6bb9fc39e748f44817a064ae0da4ac83e6a5
ShreyasKudahalli <shreyaskudahalli1857@gmail.com>
2026-05-15 12:05:13 +0530

    DB setup

--- RestaurantMenu/settings.py
+++ RestaurantMenu/settings.py
@@ -118,7 +118,7 @@ DATABASE_URL = os.environ.get("DATABASE_URL")
 
 if DATABASE_URL:
     DATABASES = {
-        'default': dj_database_url.parse(
+        'default': dj_database_url.config(
             DATABASE_URL,
             conn_max_age=600,
             ssl_require=True
